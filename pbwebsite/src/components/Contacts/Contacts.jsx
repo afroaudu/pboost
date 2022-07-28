1245d144ea520b798bacfdf9c7dc54f969a091dc
afroaudu <afroaudu01@gmail.com>
2022-07-28 19:35:47 +0100

    created the complete folder structure and started navbar

--- pbwebsite/src/components/Contacts/Contacts.jsx
+++ pbwebsite/src/components/Contacts/Contacts.jsx
@@ -2,7 +2,25 @@ import React from 'react'
 
 const Contacts = () => {
   return (
-    <div>Contacts</div>
+    <div>
+      <form action="">
+        <label htmlFor="">Full name: </label>
+        <input type="name" placeholder='write down your name' />
+
+        <label htmlFor="">Email: </label>
+        <input type="email" placeholder='write email' />
+        
+        <label htmlFor="">Phone number: </label>
+        <input type="tel" placeholder='write down your number' />
+        
+        <label htmlFor="">Subject: </label>
+        <input type="text" placeholder='write down your name' />
+        
+        <label htmlFor="">Message: </label>
+        <input type="text" placeholder='write down your name' />
+        
+      </form>
+    </div>
   )
 }
 
--- pbwebsite/src/components/Nav/Nav.jsx
+++ pbwebsite/src/components/Nav/Nav.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import '../Nav/nav.css'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <h2> <a href=""> Primaboost </a></h2>
+
+<ul>
+  <li><a href="#"> Home </a></li>
+  <li><a href="#"> Vision </a></li>
+  <li><a href="#"> Services </a></li>
+  <li><a href="#"> About </a></li>
+  <li><a href="#"> Technology</a> </li>
+  <li><a href="#"> Contact </a></li>
+  </ul>
+    </nav>
   )
-}
+  }
 
 export default Nav
