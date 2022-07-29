f653a7f90e31404953e8f988741f5b8b5bef9c6e
afroaudu <afroaudu01@gmail.com>
2022-07-29 08:30:55 +0100

    initialized tailwind

--- pbwebsite/src/components/Nav/Nav.jsx
+++ pbwebsite/src/components/Nav/Nav.jsx
@@ -4,16 +4,7 @@ import '../Nav/nav.css'
 const Nav = () => {
   return (
     <nav>
-      <h2> <a href=""> Primaboost </a></h2>
-
-<ul>
-  <li><a href="#"> Home </a></li>
-  <li><a href="#"> Vision </a></li>
-  <li><a href="#"> Services </a></li>
-  <li><a href="#"> About </a></li>
-  <li><a href="#"> Technology</a> </li>
-  <li><a href="#"> Contact </a></li>
-  </ul>
+     
     </nav>
   )
   }
